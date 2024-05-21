a49e02e2b1a573250873dacfeab7c6858cad04ed
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-05-21 15:36:35 +0300

    index

---
+++ StoredProc/StoredProc/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoredProc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- StoredProc/StoredProc/Data/StoredProcDbContext.cs
+++ StoredProc/StoredProc/Data/StoredProcDbContext.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using StoredProc;
+using StoredProc.Models;
 
 
 namespace StoredProc.Data
 {
     public class StoredProcDbContext : DbContext
-    
-        public StoredProcDbContex(DbContextOptions<StoredProcDbContext> options)
+    { 
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
             : base(options) { }
 
         public DbSet<Employee> Employees { get; set;  }
