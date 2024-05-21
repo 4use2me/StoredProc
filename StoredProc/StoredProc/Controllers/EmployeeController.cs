2501e73883ed68679f6331b0b2ef4a58c5a8e85c
WINDOWS-1DCGN01\Kasutaja <forusetome@gmail.com>
2024-05-21 16:04:20 +0300

    butten

--- StoredProc/StoredProc/Controllers/EmployeeController.cs
+++ StoredProc/StoredProc/Controllers/EmployeeController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProc.Data;
+using StoredProc.Models;
 
 namespace StoredProc.Controllers
 {
     public class EmployeeController : Controller
     {
+        public StoredProcDbContext_context;
+
+        public EmployeeController
+            (
+                StoredProcDbContext context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _comtext.Employees
+                .FormSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
         }
     }
 }
